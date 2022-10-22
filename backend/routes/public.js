f7ef2d5a4196a5b92c0a71e177f72eaff190575a
Farzan Khan <farzankhan009@gmail.com>
2022-10-22 05:16:23 +0500

    add relation table user domain and controller and routes

--- backend/routes/public.js
+++ backend/routes/public.js
@@ -5,6 +5,7 @@ const validate = require("express-validation");
 const authValidation = require("../validations/auth");
 const users = require("../controllers/users");
 const domains = require("../controllers/domains");
+const users_domains = require("../controllers/user_domain");
 const reports = require("../controllers/reports");
 
 
@@ -24,6 +25,14 @@ router.route("/domain/add").post(domains.addDomain);
 
 router.route("/domain/update/:id").put(domains.updateDomain);
 router.route("/domain/delete/:id").delete(domains.deleteDomain);
+
+router.route("/users_domains/:id").get(users_domains.getUserDomainById);
+router.route("/users_domains_by_user_id/:user_id").get(users_domains.getUserDomainByUserId);
+router.route("/users_domains").get(users_domains.getAllUserDomains);
+router.route("/user_domain/add").post(users_domains.addUserDomain);
+router.route("/user_domain/update/:id").put(users_domains.updateUserDomain);
+router.route("/user_domain/delete/:id").delete(users_domains.deleteUserDomain);
+
 
 
 router.route("/reports/new").post(reports.addReport);
